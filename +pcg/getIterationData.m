a37dbedbe877c0b0c9d83b879b657839df4df866
Damian Frick <dafrick@control.ee.ethz.ch>
2016-09-30 13:55:25 +0200

    Adding ADMM option for iteration data generation

--- +pcg/getIterationData.m
+++ +pcg/getIterationData.m
@@ -3,6 +3,7 @@ function [M, c, W, update, xi] = getIterationData(H, h, N, dims, xi, varargin)
     p.addParameter('Aeq', [], @isnumeric);
     p.addParameter('beq', [], @isnumeric);
     p.addParameter('eps', 100*eps, @isnumeric);
+    p.addParameter('method', 'lim', @(s)(any(strcmp(s, {'lim', 'admm'}))));
     p.parse(varargin{:});
     options = p.Results;
     
@@ -17,38 +18,60 @@ function [M, c, W, update, xi] = getIterationData(H, h, N, dims, xi, varargin)
     m = sum(svd(Aeq) > options.eps);
     v = Aeq\beq;
 
-    K = [H Aeq'; Aeq zeros(size(Aeq,1),size(Aeq,1))]^(-1);
-    R = K(1:dims.nn,1:dims.nn);
-    % Checking whether xi satisfies xi > 1/[smallest positive eigenvalue of R]
-    ximin = 1/min(subsref(sort(eig(R),1,'descend'), struct('type', '()', 'subs', {{1:m}})));
-    if xi <= ximin
-        warning('getIterationData:InvalidProximalScaling', ['The proximal scaling xi is not large enough, needs to be larger than ' num2str(ximin) '. Setting to xi ' num2str(ximin*2) ' and proceeding']);
-        xi = 2*ximin;
+    switch options.method
+        case 'lim',
+            K = [H Aeq'; Aeq zeros(size(Aeq,1),size(Aeq,1))]^(-1);
+            R = K(1:dims.nn,1:dims.nn);
+            % Checking whether xi satisfies xi > 1/[smallest positive eigenvalue of R]
+            ximin = 1/min(subsref(sort(eig(R),1,'descend'), struct('type', '()', 'subs', {{1:m}})));
+            if xi <= ximin
+                warning('getIterationData:InvalidProximalScaling', ['The proximal scaling xi is not large enough, needs to be larger than ' num2str(ximin) '. Setting to xi ' num2str(ximin*2) ' and proceeding']);
+                xi = 2*ximin;
+            end
+            M = xi*((xi*R-eye(size(R)))\R);
+            c = (xi*R-eye(size(R)))\(R*(h+H*v)-v);
+            % Compute weigthing matrix W
+            [V,D] = eig((M+M')/2); % Ensure M is symmetric (small asymmetry can lead to large errors!)
+            d = diag(D);
+            [~,i] = sort(real(d));
+            d = 2*d;
+            d(i(1:m)) = -1;
+            d = 1./d;
+            W = V*diag(d)*V';
+            W = real(W);
+            
+            % Sparsify
+            R(abs(R)< options.eps) = 0;
+            R = sparse(R);
+            H(abs(H)< options.eps) = 0;
+            H = sparse(H);
+            M(abs(M)< options.eps) = 0;
+            M = sparse(M);
+            W(abs(W)< options.eps) = 0;
+            W = sparse(W);
+
+            % Update function for changing h
+            update = @(hn,b) (xi*R-eye(size(R)))\(R*(h+hn+H*(Aeq\b))-(Aeq\b));  
+        case 'admm'
+            K = [H+xi*eye(size(H)) Aeq'; Aeq zeros(size(Aeq,1),size(Aeq,1))]^(-1);
+            R = K(1:dims.nn,1:dims.nn);
+            M = R;
+            c = -R*(h+(H+xi*eye(size(H)))*v)+v;
+            W = xi*eye(size(M));
+            
+            % Sparsify
+            R(abs(R)< options.eps) = 0;
+            R = sparse(R);
+            H(abs(H)< options.eps) = 0;
+            H = sparse(H);
+            M(abs(M)< options.eps) = 0;
+            M = sparse(M);
+            W(abs(W)< options.eps) = 0;
+            W = sparse(W);
+
+            % Update function for changing h
+            update = @(hn,b) (-R*(h+hn+(H+xi*eye(size(H)))*(Aeq\b))+(Aeq\b));
     end
-    M = xi*((xi*R-eye(size(R)))\R);
-    c = (xi*R-eye(size(R)))\(R*(h+H*v)-v);
-    % Compute weigthing matrix W
-    [V,D] = eig((M+M')/2); % Ensure M is symmetric (small asymmetry can lead to large errors!)
-    d = diag(D);
-    [~,i] = sort(real(d));
-    d = 2*d;
-    d(i(1:m)) = -1;
-    d = 1./d;
-    W = V*diag(d)*V';
-    W = real(W);
-    
-    % Sparsify
-    R(abs(R)< options.eps) = 0;
-    R = sparse(R);
-    H(abs(H)< options.eps) = 0;
-    H = sparse(H);
-    M(abs(M)< options.eps) = 0;
-    M = sparse(M);
-    W(abs(W)< options.eps) = 0;
-    W = sparse(W);
-    
-    % Update function for changing h
-    update = @(hn) (xi*R-eye(size(R)))\(R*(hn+H*v)-v);
 
 end
 
